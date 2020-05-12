88488eaf7acf9f4f257fae8454e44fe734167605
Hazumi Otoya <mail@anpuraya.com>
2020-05-13 04:33:18 +0900

    fixed authentication function

--- src/main.js
+++ src/main.js
@@ -27,7 +27,6 @@ Vue.config.productionTip = false
 var firebaseConfig = {
   apiKey: "AIzaSyAYHxfofkeKOnMjr_8HsdzXboJEwHY1RxM",
   authDomain: "project-localmemo.firebaseapp.com",
-  databaseURL: "https://project-localmemo.firebaseio.com",
   databaseURL: "https://mndkproject.github.io/local_memo/",
   projectId: "project-localmemo",
   storageBucket: "project-localmemo.appspot.com",
