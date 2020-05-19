371fa55b289698b2583efea277749b897c7ccaa6
Hazumi Otoya <mail@anpuraya.com>
2020-05-19 13:31:52 +0900

    Updates

---
+++ precache-manifest.8cb9ab30949dc33c7a52a3ab2d33aac8.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b93d2864fcc2629d7e3",
+    "url": "css/app.1eb6e113.css"
+  },
+  {
+    "revision": "b6d3c7d7febb707d6708",
+    "url": "css/chunk-vendors.cadda6db.css"
+  },
+  {
+    "revision": "b1149df5f464147599ef6c7b324e88a0",
+    "url": "favicon.png"
+  },
+  {
+    "revision": "a4d31128b633bc0b1cc1f18a34fb3851",
+    "url": "fonts/Material-Design-Iconic-Font.a4d31128.woff2"
+  },
+  {
+    "revision": "b351bd62abcd96e924d9f44a3da169a7",
+    "url": "fonts/Material-Design-Iconic-Font.b351bd62.ttf"
+  },
+  {
+    "revision": "d2a55d331bdd1a7ea97a8a1fbb3c569c",
+    "url": "fonts/Material-Design-Iconic-Font.d2a55d33.woff"
+  },
+  {
+    "revision": "2ef8ba3410dcc71578a880e7064acd7a",
+    "url": "fonts/fa-brands-400.2ef8ba34.woff"
+  },
+  {
+    "revision": "5e2f92123d241cabecf0b289b9b08d4a",
+    "url": "fonts/fa-brands-400.5e2f9212.woff2"
+  },
+  {
+    "revision": "98b6db59be947f563350d2284fc9ea36",
+    "url": "fonts/fa-brands-400.98b6db59.ttf"
+  },
+  {
+    "revision": "a7b95dbdd87e0c809570affaf366a434",
+    "url": "fonts/fa-brands-400.a7b95dbd.eot"
+  },
+  {
+    "revision": "427d721b86fc9c68b2e85ad42b69238c",
+    "url": "fonts/fa-regular-400.427d721b.woff"
+  },
+  {
+    "revision": "65b9977aa23185e8964b36eddbce7a20",
+    "url": "fonts/fa-regular-400.65b9977a.ttf"
+  },
+  {
+    "revision": "dcce4b7fbd5e895561e18af4668265af",
+    "url": "fonts/fa-regular-400.dcce4b7f.eot"
+  },
+  {
+    "revision": "e6257a726a0cf6ec8c6fec22821c055f",
+    "url": "fonts/fa-regular-400.e6257a72.woff2"
+  },
+  {
+    "revision": "418dad87601f9c8abd0e5798c0dc1feb",
+    "url": "fonts/fa-solid-900.418dad87.woff2"
+  },
+  {
+    "revision": "46e7cec623d8bd790d9fdbc8de2d3ee7",
+    "url": "fonts/fa-solid-900.46e7cec6.eot"
+  },
+  {
+    "revision": "a7140145ebaaf5fb14e40430af5d25c4",
+    "url": "fonts/fa-solid-900.a7140145.woff"
+  },
+  {
+    "revision": "ff8d9f8adb0d09f11d4816a152672f53",
+    "url": "fonts/fa-solid-900.ff8d9f8a.ttf"
+  },
+  {
+    "revision": "7f9fdd5e7c0656fea97141765f3e0b50",
+    "url": "fonts/ionicons.7f9fdd5e.eot"
+  },
+  {
+    "revision": "96f1c901c087fb64019f7665f7f8aca6",
+    "url": "fonts/ionicons.96f1c901.woff2"
+  },
+  {
+    "revision": "c37ad37a3a23417b739ac3b297416201",
+    "url": "fonts/ionicons.c37ad37a.woff"
+  },
+  {
+    "revision": "cfdc15225683b7529d6ba1e9d8a9be59",
+    "url": "fonts/ionicons.cfdc1522.ttf"
+  },
+  {
+    "revision": "99d9fcd6fb0f4fef2a6ed13f07537d6d",
+    "url": "images/icons/icon-128x128.png"
+  },
+  {
+    "revision": "83fbe2049da608bec35ba3972b819d08",
+    "url": "images/icons/icon-144x144.png"
+  },
+  {
+    "revision": "df721d20250cd17edab71a793e4fc17e",
+    "url": "images/icons/icon-152x152.png"
+  },
+  {
+    "revision": "0f8807dddb5a6c53698367c06d6d0d43",
+    "url": "images/icons/icon-192x192.png"
+  },
+  {
+    "revision": "221f19504cac53d92519e661acc93e08",
+    "url": "images/icons/icon-384x384.png"
+  },
+  {
+    "revision": "9bd0a1752ab48d1c9b2ea644e8352a3f",
+    "url": "images/icons/icon-512x512.png"
+  },
+  {
+    "revision": "b1149df5f464147599ef6c7b324e88a0",
+    "url": "images/icons/icon-72x72.png"
+  },
+  {
+    "revision": "b4030fd7649d388cc59d2054dfca8f46",
+    "url": "images/icons/icon-96x96.png"
+  },
+  {
+    "revision": "5bf145531213545e03ff41cd27df7d2b",
+    "url": "img/fa-brands-400.5bf14553.svg"
+  },
+  {
+    "revision": "5eb754ab7dbd2fee562360528db4c3c0",
+    "url": "img/fa-regular-400.5eb754ab.svg"
+  },
+  {
+    "revision": "49279363201ed19a840796e05a74a91b",
+    "url": "img/fa-solid-900.49279363.svg"
+  },
+  {
+    "revision": "665921072642ed354618b32af7425a22",
+    "url": "img/ionicons.66592107.svg"
+  },
+  {
+    "revision": "5272a65d9b71b8708df5816e3a143ea6",
+    "url": "index.html"
+  },
+  {
+    "revision": "0b93d2864fcc2629d7e3",
+    "url": "js/app.583430b0.js"
+  },
+  {
+    "revision": "b6d3c7d7febb707d6708",
+    "url": "js/chunk-vendors.eb4e9e35.js"
+  },
+  {
+    "revision": "3cc044510a8b3d69ccca0b478724b151",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.3d5a5b4f4ff48e64bcc68905dcb36950.js"
+  "precache-manifest.50439d905191bea949912ae25d8f86fa.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "local-memo"});
